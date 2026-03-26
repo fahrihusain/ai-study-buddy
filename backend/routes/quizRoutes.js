2d37e21cb9a6b92632f0cb08be7569b1b8b65933
fahri <fahrykhusain77@gmail.com>
2026-03-26 20:54:25 +0700

    fix: server.js dan routes/quizRoutes.js

---
+++ backend/routes/quizRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getQuizzes, getQuizById, submitQuiz, getQuizResults, deleteQuiz } from "../controllers/quizController.js";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+//All router are protected
+router.use(protect);
+
+router.get("/:documentId", getQuizzes);
+router.get("/quiz/:id", getQuizById);
+router.post("/:id/submit", submitQuiz);
+router.get("/:id/results", getQuizResults);
+router.delete("/:id", deleteQuiz);
+
+export default router;
