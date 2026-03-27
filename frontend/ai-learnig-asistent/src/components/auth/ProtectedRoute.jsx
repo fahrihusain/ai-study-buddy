c0eca25a57603d217a85e48ee96557bbb0832190
fahri <fahrykhusain77@gmail.com>
2026-03-27 12:57:39 +0700

    fix: ProtectedRoute.jsx

--- frontend/ai-learnig-asistent/src/components/auth/ProtectedRoute.jsx
+++ frontend/ai-learnig-asistent/src/components/auth/ProtectedRoute.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
+import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = () => {
-  const isAuthenticated = true; // Replace with actual authentication logic
-  const loading = false; // Replace with actual loading state
+  const { isAuthenticated, loading } = useAuth();
 
   if (loading) {
     return <div className="">Loading...</div>;
