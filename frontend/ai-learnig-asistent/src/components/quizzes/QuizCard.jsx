c278a1222f1b20b9583beb99ffe19d9c5bf9bc3f
fahri <fahrykhusain77@gmail.com>
2026-04-03 16:55:58 +0700

    fix: QuizManager.jsx, EmptyState.jsx, DocumentDetailPage.jsx, QuizCard.jsx

---
+++ frontend/ai-learnig-asistent/src/components/quizzes/QuizCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuizCard = ({ quiz, onDelete }) => {
+  return <div>QuizCard</div>;
+};
+
+export default QuizCard;
--- frontend/ai-learnig-asistent/src/pages/Documents/DocumentDetailPage.jsx
+++ frontend/ai-learnig-asistent/src/pages/Documents/DocumentDetailPage.jsx
@@ -9,6 +9,7 @@ import Tabs from "../../components/common/Tabs";
 import ChatInterface from "../../components/chat/ChatInterface";
 import AIActions from "../../components/ai/AiActions";
 import FlashcardManager from "../../components/flashcards/FlashcardManager";
+import QuizManager from "../../components/quizzes/QuizManager";
 
 const DocumentDetailPage = () => {
   const { id } = useParams();
@@ -98,7 +99,7 @@ const DocumentDetailPage = () => {
   };
 
   const renderQuizzesTab = () => {
-    return " renderQuizzesTab";
+    return <QuizManager documentId={id} />;
   };
 
   const tabs = [
